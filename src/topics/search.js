9eb26c9bff737e5a4bd108492e665c720053438e
cindywgg <cindywan@andrew.cmu.edu>
2025-02-27 15:53:53 -0500

    fix: resolved all test issues

--- src/topics/search.js
+++ src/topics/search.js
@@ -5,16 +5,28 @@ module.exports = function (Topics) {
 		const keyword = query.keyword || '';
 		const initTopics = query.initTopics || null;
 
+<<<<<<< HEAD
         if (initTopics) {
             const topicsByKeyword = initTopics.filter((topic) => {
                 const title = topic.title.toLowerCase();
                 return title.includes(keyword);
             });
+=======
+		if (initTopics) {
+			const topicsByKeyword = initTopics.filter((topic) => {
+				const title = topic.title.toLowerCase();
+				return title.includes(keyword);
+			});
+>>>>>>> 81b4196 (fix: resolved all test issues)
 
-            const searchResult = { topics: topicsByKeyword };
-            return searchResult;
-        }
+			const searchResult = { topics: topicsByKeyword };
+			return searchResult;
+		}
 
+<<<<<<< HEAD
         return { topics: initTopics };
+=======
+		return { topics: initTopics };
+>>>>>>> 81b4196 (fix: resolved all test issues)
 	};
 };
