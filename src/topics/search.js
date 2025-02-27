14c0dc62b28a522648d3c0aa89bb369d5aaed94c
cindywgg <cindywan@andrew.cmu.edu>
2025-02-27 16:46:03 -0500

    feat: implemented backend api support for keyword-based search mechanism

---
+++ src/topics/search.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function (Topics) {
+	Topics.searchByKeyword = async function (query) {
+		const keyword = query.keyword || '';
+		const initTopics = query.initTopics || null;
+
+		if (initTopics) {
+			const topicsByKeyword = initTopics.filter((topic) => {
+				const title = topic.title.toLowerCase();
+				return title.includes(keyword);
+			});
+
+			const searchResult = { topics: topicsByKeyword };
+			return searchResult;
+		}
+
+		return { topics: initTopics };
+	};
+};
