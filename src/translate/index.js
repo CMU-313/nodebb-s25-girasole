61c22d004a09f573ed3dc0590cc1387601e03bad
Ethan Wang <ethanwan@andrew.cmu.edu>
2025-04-05 23:19:23 -0400

    Fix: fixing translation api link

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,7 @@ var request = require('request');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = "http://translator:5000/";
+	const TRANSLATOR_API = "http://128.2.205.168:5000";
 	try {
 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
 		const data = await response.json();
